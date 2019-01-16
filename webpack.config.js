9f71c99bba609004e52163360a11b3e7ed0a837a
Antony <antongolova@gmail.com>
2019-01-16 23:13:35 +0300

    envierment

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,35 @@
-const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const stylus_plugin = require('stylus_plugin');
 
-exports.plugins.push(
-  new VuetifyLoaderPlugin({
-    /**
-     * This function will be called for every tag used in each vue component
-     * It should return an array, the first element will be inserted into the
-     * components array, the second should be a corresponding import
-     *
-     * originalTag - the tag as it was originally used in the template
-     * kebabTag    - the tag normalised to kebab-case
-     * camelTag    - the tag normalised to PascalCase
-     * path        - a relative path to the current .vue file
-     * component   - a parsed representation of the current component
-     */
-    match (originalTag, { kebabTag, camelTag, path, component }) {
-      if (kebabTag.startsWith('core-')) {
-        return [camelTag, `import ${camelTag} from '@/components/core/${camelTag.substring(4)}.vue'`]
-      }
-    }
-  })
-)
+module.exports = {
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            }
+        ],
+        loaders: [
+            { test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader' }
+        ],
+        stylus: {
+            use: [stylus_plugin()]
+        }
+    },
+    plugins: [
+        // убедитесь что подключили плагин!
+        new VueLoaderPlugin()
+    ]
+}
