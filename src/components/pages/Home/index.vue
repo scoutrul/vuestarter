6bafc518d59eaedebfb99abcbb034ffb72f4a1f3
Anton <antongolova@gmail.com>
2019-02-01 17:28:50 +0300

    layout fix and svg plashka

--- src/components/pages/Home/index.vue
+++ src/components/pages/Home/index.vue
@@ -1,5 +1,19 @@
 <template>
-    <div>Главная</div>
+    <div>
+        <svg
+            width="7688"
+            height="151"
+            viewBox="0 0 7688 151"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            id="svg_arc"
+        >
+            <path
+                d="M7688 0C6560.46 95.5195 5246.64 150.3 3844 150.3C2441.36 150.3 1127.54 95.5195 -0.000244141 0H7688Z"
+                fill="#00BAFF"
+            />
+        </svg>
+    </div>
 </template>
 
 <script>
@@ -7,4 +21,23 @@ export default {
     data: () => ({}),
 };
 </script>
-<style></style>
+<style>
+#svg_arc {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 100%;
+    min-width: 1000px;
+}
+</style>
+
+<!--<svg-->
+<!--width="1920"-->
+<!--height="547"-->
+<!--viewBox="0 0 1920 547"-->
+<!--fill="none"-->
+<!--xmlns="http://www.w3.org/2000/svg"-->
+<!--&gt;-->
+<!--<ellipse cx="994.5" cy="273.5" rx="1898.5" ry="273.5" fill="white"/>-->
+<!--</svg>-->
+<!--<path d="”" M-904,274a1899,234 0 1,0 3798,0a1899,234 0 1,0 -3798,0 “ />-->
