ce21a3eee2b2ee3b2e87a1fc29e1e2412d212c74
Anton <antongolova@gmail.com>
2019-02-04 13:37:40 +0300

    blocks w arc

--- src/components/blocks/Content/Content.vue
+++ src/components/blocks/Content/Content.vue
@@ -1,19 +1,42 @@
 <template>
-    <div>
-        <svg
-            id="svg_arc"
-            width="7688"
-            height="151"
-            viewBox="0 0 7688 151"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <path
-                d="M7688 0C6560.46 95.5195 5246.64 150.3 3844 150.3C2441.36 150.3 1127.54 95.5195 -0.000244141 0H7688Z"
-                fill="#00BAFF"
-            />
-        </svg>
-    </div>
+    <v-layout
+        column
+        :style="{
+            background: bgColor,
+        }"
+    >
+        <template v-if="isTop | isTopReverse">
+            <svg
+                :class="['arc', 'arc_top', isTopReverse && 'reverse']"
+                width="7688"
+                height="151"
+                viewBox="0 0 7688 151"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    d="M7688 0C6560.46 95.5195 5246.64 150.3 3844 150.3C2441.36 150.3 1127.54 95.5195 -0.000244141 0H7688Z"
+                    :fill="isTopReverse ? bgColor : '#FFFFFF'"
+                />
+            </svg>
+        </template>
+        <v-layout> <slot></slot> </v-layout>
+        <template v-if="isBottom | isBottomReverse">
+            <svg
+                :class="['arc', 'arc_bottom', isBottomReverse && 'reverse']"
+                width="7688"
+                height="151"
+                viewBox="0 0 7688 151"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    d="M7688 0C6560.46 95.5195 5246.64 150.3 3844 150.3C2441.36 150.3 1127.54 95.5195 -0.000244141 0H7688Z"
+                    :fill="isBottomReverse ? bgColor : '#FFFFFF'"
+                />
+            </svg>
+        </template>
+    </v-layout>
 </template>
 
 <script>
@@ -31,12 +54,29 @@ export default {
     },
 };
 </script>
-<style scoped>
-#svg_arc {
-    position: absolute;
-    left: 50%;
-    transform: translateX(-50%);
+
+<style scoped lang="stylus">
+.arc {
     width: 100%;
-    min-width: 1000px;
+    height auto
+    path {
+        transform-origin center
+    }
+    &_top path {
+        transform: rotateX(0deg);
+    }
+    &_bottom path {
+            transform: rotateX(-180deg);
+    }
+    &.reverse {
+        background #FFF
+        &.arc_top path {
+                transform: rotateX(-180deg);
+
+        }
+        &.arc_bottom path {
+                transform: rotateX(0deg);
+        }
+    }
 }
 </style>
--- src/components/pages/Home/index.vue
+++ src/components/pages/Home/index.vue
@@ -1,12 +1,10 @@
 <template>
-    <v-layout>
-        <Content
-            is-top
-            is-bottom
-            is-top-reverse
-            is-bottom-reverse
-            bg-color="#FCD"
-        />
+    <v-layout column>
+        <Content is-top is-bottom bg-color="#00BAFF">sec</Content>
+        <Content is-top-reverse is-bottom-reverse bg-color="#00BAFF"
+            >first</Content
+        >
+        <Content bg-color="#00BAFF">3333333</Content>
     </v-layout>
 </template>
 
