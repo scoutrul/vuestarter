c82eb70696d3bd07d8be712472f99e8e9528ba8c
Anton <scoutrul@mail.ru>
2019-03-09 01:50:00 +0300

    unirest api

--- src/components/pages/Home/index.vue
+++ src/components/pages/Home/index.vue
@@ -1,10 +1,29 @@
 <template>
-    <div>Home page</div>
+    <div class="container">
+        <h1>Home page</h1>
+        <div>
+            <div v-for="league of leagues" :key="league.league_id">{{league.name}}</div>
+        </div>
+    </div>
 </template>
 
 <script>
+import unirest from 'unirest';
+
 export default {
-    data: () => ({}),
+    data: () => ({
+        leagues: {}
+    }),
+    mounted(){
+         unirest.get("https://api-football-v1.p.rapidapi.com/leagues")
+            .header("X-RapidAPI-Key", "c4e412ce52msh6c646cd1fe5ce0ap199220jsn0b49016530ce")
+            .end(result => {
+                this.leagues = result.body.api.leagues || {};
+                console.log(result.status);
+                console.log(result.headers);
+                console.log(result.body);
+            });
+    }
 };
 </script>
 <style></style>
