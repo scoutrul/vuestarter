05fdf1e5a7b56252a464c007e7a1f9f2ef1b2ae1
Anton <antongolova@gmail.com>
2019-03-11 12:52:35 +0300

    cleaning

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <v-app>
         <Main>
-            <Header ref="header" :style="getHeaderHeight">
+            <Header ref="header">
                 <Container class="header__container">
                     <LogoHeader />
                     <Menu />
@@ -12,19 +12,16 @@
                     <router-view />
                 </v-fade-transition>
             </Body>
-            
+            <Menu />
         </Main>
     </v-app>
 </template>
 
 <script>
-import { Main, Header, Body, Container, Footer } from './components/layout';
+import { Main, Header, Body, Container } from './components/layout';
 import {
     LogoHeader,
-    LogoFooter,
     Menu,
-    Copyrights,
-
 } from './components/blocks';
 
 export default {
@@ -33,67 +30,19 @@ export default {
         Main,
         Container,
         Header,
-        Footer,
         Body,
         Menu,
         LogoHeader,
-        LogoFooter,
-        Copyrights,
     },
 
     data() {
         return {
-            pageYOffset: 0,
-            innerHeight: 0,
-            footerHeight: 0,
-            headerHeight: 0,
-            appHeight: 0,
-            headerSize: {
-                small: `64px`,
-                big: `128px`,
-            },
-        };
-    },
-    computed: {
-        getHeaderHeight() {
-            return {
-                height: this.isHeaderScrolled()
-                    ? this.headerSize.small
-                    : this.headerSize.big,
-            };
-        },
-        footerAllowPages() {
-            const pagesWithFooter = ['home', 'profile'];
-            return pagesWithFooter.includes(this.$route.name);
-        },
-    },
-    mounted() {
-        this.headerHeight = this.$refs.header.$el.clientHeight;
-        this.footerHeight = this.$refs.footer.$el.clientHeight;
-        this.appHeight = this.$el.clientHeight;
-        this.innerHeight = window.innerHeight;
-        this.registerHandlers();
-    },
-
-    methods: {
-        isHeaderScrolled() {
-            return this.pageYOffset >= this.headerHeight;
-        },
-        isFooterShow() {
-            return (
-                this.pageYOffset >=
-                    this.appHeight - this.footerHeight - this.innerHeight ||
-                this.appHeight === this.innerHeight
-            );
-        },
-
+            }
     },
 };
 </script>
 
 <style lang="stylus">
 @import '~@/assets/styles/index.styl';
-.footer-display-none {
-    display none
-}
+
 </style>
