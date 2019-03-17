9e91c7e065394f8b8378c27ad004965ae49c0a85
Anton <scoutrul@mail.ru>
2019-03-18 00:27:42 +0300

    note

--- src/components/pages/Home/index.vue
+++ src/components/pages/Home/index.vue
@@ -1,5 +1,16 @@
 <template>
-    <div class="container"><h1>Home page</h1></div>
+    <div class="container">
+        <h1>Home page</h1>
+        <v-layout>
+            <pre>
+                - Компонент показа team vs team
+                    - взять с сетки лайва
+                    - итерейтить этот компонент
+                - Компонент получения и показа событий матча
+                - Компонент получения и показа статистики
+            </pre>
+        </v-layout>    
+    </div>
 </template>
 
 <script>
