ad6d6cd12214a072b3695d9ec2feeee530632864
Anton <scoutrul@mail.ru>
2019-03-17 03:26:46 +0300

    podgruzka img logos

--- src/components/pages/Live/index.vue
+++ src/components/pages/Live/index.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="container">
-    <h1>Live</h1>
-    <div v-if="api.results > 0">
-      <v-layout column v-for="fixture of api.fixtures" :key="fixture.fixture_id" class="goal--event">
+    <h1>Live {{fixtures.results}}</h1>
+    <div>
+      <v-layout column v-for="fixture of fixtures" :key="fixture.fixture_id" class="goal--event">
         <v-layout class="goal--center goal--round">{{fixture.round}}</v-layout>
         <v-layout>
           <v-flex xs5 class="goal--left goal--name">
             <div class="goal--center">
               {{fixture.homeTeam}}
               <br>
-              <img :src="getLogoSrc(fixture.homeTeam_id)" alt height="80px">
+              <img
+                :src="getLogoSrc(fixture.homeTeam_id)"
+                alt
+                height="80px"
+              >
             </div>
           </v-flex>
           <v-layout class="goal--center" column>
@@ -23,13 +27,15 @@
             <div class="goal--center">
               {{fixture.awayTeam}}
               <br>
-              <img :src="getLogoSrc(fixture.awayTeam_id)" alt height="80px">
+              <img
+                :src="getLogoSrc(fixture.awayTeam_id)"
+                alt
+                height="80px"
+              >
             </div>
           </v-flex>
         </v-layout>
-        <v-layout class="goal--center goal--round">
-            <router-link :to="{ name: 'fixture', params: {id: fixture.fixture_id}}">Статистика матча</router-link>
-        </v-layout>
+        <v-layout class="goal--center goal--round">Статистика</v-layout>
       </v-layout>
     </div>
   </div>
@@ -39,77 +45,73 @@
 import unirest from 'unirest';
 import api from '@/services/';
 import forEach from 'lodash/forEach';
+import map from 'lodash/map';
 import assign from 'lodash/assign';
+import assignIn from 'lodash/assignIn';
+
 import find from 'lodash/find';
 import mapKeys from 'lodash/mapKeys';
-import concat from 'lodash/concat';
 
 export default {
   data: () => ({
-      api: {
-        fixtures: {
-            results: 0,
-        },
-    },
-    teams: {},
-    logos: [],
-    logoIsLoaded: false,
+    fixtures: {},
+    logos: {},
+    logosHaveResolved: false,
   }),
-  beforeMount() {
+  mounted() {
     api
       .getApi('fixtures/live')
       .then(res => {
-        this.api = { ...this.api, ...res.body.api};
+        this.fixtures = res.body.api.fixtures;
       })
-      .then(this.getTeamsLogos());
-  },
-  mounted() {
-
+      .then(this.getTeamsLogos);
   },
   computed: {
-    getLogoSrc() {
-        return id => {
-            try {
-                // const findLogo = find(this.logos, logo => logo.team_id == id);
-                return find(this.logos, logo => logo.team_id == id).logo;
-            }
-            catch(e){
-                console.log(e);
-                return '';
-            }
-        }
+    // getLogoSrc() {
+    //   return id =>
+    //     (this.logos[id] && this.logos[id].logo) ||
+    //     'http://www.clker.com/cliparts/X/7/N/j/x/Z/blank-shield-soccer.svg';
+    // },
+  },
+  watch: {
+    logos(val) {
+      console.log(val);
+    },
+    logosHaveResolved(val) {
+      console.log(val);
     },
   },
 
   methods: {
+    getLogoSrc(id) {
+      return !this.logosHaveResolved
+        ? 'http://www.clker.com/cliparts/X/7/N/j/x/Z/blank-shield-soccer.svg'
+        : this.logos[id] && this.logos[id].logo || 'http://www.clker.com/cliparts/X/7/N/j/x/Z/blank-shield-soccer.svg';
+    },
     getTeamsLogos() {
-      this.logoIsLoaded = false;
-      let promises = [];
-      forEach(this.fixtures, (fixture, key) => {
-        promises.push(
-          api.getTeamLogo(fixture.homeTeam_id).then(res => {
-            this.logos = concat(
-              this.logos,
-              res.body.api.teams[fixture.homeTeam_id],
-            );
-          }),
-        );
-        promises.push(
-          api.getTeamLogo(fixture.awayTeam_id).then(res => {
-            this.logos = concat(
-              this.logos,
-              res.body.api.teams[fixture.awayTeam_id],
-            );
-          }),
-        );
-      });
-      Promise.all(promises)
+      this.logosHaveResolved = false;
+      const logoArr = this.logos;
+      Promise.all([
+        ...map(this.fixtures, async fixture => {
+          await api.getTeamLogo(fixture.homeTeam_id).then(res => {
+            logoArr[fixture.homeTeam_id] =
+              res.body.api.teams[fixture.homeTeam_id];
+          });
+        }),
+        ...map(this.fixtures, async fixture => {
+          await api.getTeamLogo(fixture.homeTeam_id).then(res => {
+            logoArr[fixture.homeTeam_id] =
+              res.body.api.teams[fixture.homeTeam_id];
+          });
+        }),
+      ])
         .then(res => {
-            this.logoIsLoaded = true;
-            console.log('done')
+          console.log('done');
+          console.log(logoArr);
+          this.logos = logoArr;
+          this.logosHaveResolved = true;
         })
         .catch(reason => {
-          this.logoIsLoaded = true;  
           console.log(reason);
         });
     },
@@ -144,7 +146,7 @@ export default {
   margin: 10px 0;
 }
 .goal--event {
-    margin-bottom: 20px;
-    background-color: #dfe2e2;
+  margin-bottom: 20px;
+  background-color: #dfe2e2;
 }
 </style>
